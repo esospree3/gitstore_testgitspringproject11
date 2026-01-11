36ef54bc0bc5767e1a47eb987d6eb8837928bfe4
esosp <esospree333@gmail.com>
2026-01-11 14:48:59 -0500

    first commit

---
+++ src/main/java/com/git/test/example11/gitTest11/Repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.git.test.example11.gitTest11.Repository;
+
+
+import com.git.test.example11.gitTest11.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+  //  List<Product> findByCategory(String category);
+
+}
