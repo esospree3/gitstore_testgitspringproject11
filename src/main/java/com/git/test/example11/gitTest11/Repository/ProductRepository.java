21db78f04d18e727008ea4643b17726303e07813
esosp <esospree333@gmail.com>
2026-01-11 14:59:00 -0500

    commit for removal of unnecessary code like import statemnet for java.util.List

--- src/main/java/com/git/test/example11/gitTest11/Repository/ProductRepository.java
+++ src/main/java/com/git/test/example11/gitTest11/Repository/ProductRepository.java
@@ -5,8 +5,6 @@ import com.git.test.example11.gitTest11.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
   //  List<Product> findByCategory(String category);
